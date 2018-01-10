ff368fc80b83fddf40aea73914c6d7d9319fc3d7
Annammal <annalsam165@gmail.com>
2018-01-10 15:01:12 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,10 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: localhost,
+      host: "localhost",
       port: 8545,
-      gas: 6712388,
-      gasPrice: 65000000000,
       network_id: "*" // Match any network id
     }
   }
